9f363cf85a040c7a77b6b40a4cfbdf1e1a20d6a4
bw4sz <benweinstein2010@gmail.com>
2016-10-01 19:40:53 -0700

    skeleton of new project

---
+++ Amazon/Job.bash
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+#clean temp file for memory utility
+rm -rf /var/tmp/aws-mon.bak
+mv /var/tmp/aws-mon /var/tmp/aws-mon.bak
+
+#clone
+git clone git@github.com:bw4sz/Whales.git -b NOBETA --single-branch --depth 1
+
+cd Whales||sudo halt
+
+#unzip data file if needed
+#cd OutData
+#unzip pars.zip
+#cd ..
+
+#make new branch
+#name it the instance ID
+iid=$(ec2metadata --instance-id)
+
+git checkout -b $iid
+
+#render script
+Rscript -e "rmarkdown::render('SingleSpecies.Rmd')" &> run.txt
+
+#push results
+git add --all
+git commit -m "ec2 run complete"
+git push -u origin $iid
+
+#kill instance
+sudo halt
---
+++ Amazon/Startup.bash
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# spawn instance and store id
+instance_id=$(aws ec2 run-instances --image-id ami-8ac313ea --security-group-ids sg-923a98f6 --count 1 --instance-type r3.large --key-name rstudio --instance-initiated-shutdown-behavior stop --query 'Instances[0].{d:InstanceId}' --output text --iam-instance-profile Name="Ben")
+
+# wait until instance is up and running
+aws ec2 wait instance-running --instance-ids $instance_id
+
+#add name tag
+aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=Whales
+
+#cloudwatch monitor
+aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Alarm when CPU drops below 2 over 10 minutes%" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 2 --comparison-operator LessThanThreshold  --dimensions Name=InstanceId,Value=$instance_id --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:477056371121:Instance_is_idle --unit Percent
+aws cloudwatch put-metric-alarm --alarm-name memory --alarm-description "Alarm when Memory rate exceeds 90%%" --metric-name MemoryUtilization --namespace System/Linux --statistic Average --period 300 --threshold 90 --comparison-operator GreaterThanThreshold  --dimensions Name=InstanceId,Value=$instance_id --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:477056371121:Instance_is_idle --unit Percent
+
+# retrieve public dns
+dns=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[*].Instances[*].PublicDnsName' --output text | grep a)
+
+#Wait for port to be ready, takes about a minute.
+sleep 60
+
+# copy over Job.bash to instance
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i '/c/Users/Ben/.ssh/rstudio.pem' /c/Users/Ben/Documents/Whales/Amazon/Job.bash ubuntu@$dns:~
+
+# run job script on instance, don't wait for finish and disconnect terminal
+nohup ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "/c/Users/Ben/.ssh/rstudio.pem" ubuntu@$dns "nohup ./Job.bash" &
