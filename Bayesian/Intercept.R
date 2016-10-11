27eeac3d4fcba6835961b29e43484992c18afaa2
bw4sz <benweinstein2010@gmail.com>
2016-10-10 23:46:59 -0700

    struggling with conditional probability

--- Bayesian/Intercept.R
+++ Bayesian/Intercept.R
@@ -6,12 +6,14 @@ model{
 
     for (cell in 1:cells){
       
-      beh[cell] ~ dbern(X[cell] * rho + 0.0001)
+      beh[cell] ~ dbern(z[cell])
+
+      z[cell]=X[cell] * rho + 0.00001
 
       X[cell] ~ dbern(phi)
     }
 
-    z= rho * phi
+    m=mean(z[])
 
     rho ~ dbeta(1,1)
     phi ~ dbeta(1,1)
